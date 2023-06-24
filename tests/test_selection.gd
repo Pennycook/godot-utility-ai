030a53467512e536583a923d5930c20d898279e8
John Pennycook <john.pennycook@gmail.com>
2023-06-24 13:31:43 -0700

    Tidy up examples directory
    hp.tres and mp.tres weren't used anywhere, so can be removed.
    
    escape.tres and heal.tres were used for tests, so can be moved to
    tests/.
    
    Closes #1.

--- tests/test_selection.gd
+++ tests/test_selection.gd
@@ -4,13 +4,12 @@ extends GutTest
 
 
 func test_selection():
-
 	var context := {
 		"hp": 0.1,
 		"mp": 1.0,
 	}
-	var Heal := preload("res://examples/behaviors/heal.tres")
-	var Escape := preload("res://examples/behaviors/escape.tres")
+	var Heal := preload("res://tests/behaviors/heal.tres")
+	var Escape := preload("res://tests/behaviors/escape.tres")
 	var options: Array[UtilityAIOption] = [
 		UtilityAIOption.new(Heal, context),
 		UtilityAIOption.new(Escape, context),
